c53b461acd6f3c52267794f61c391310cd9d0385
Lakshay <dmslakshay@gmail.com>
2024-01-01 14:04:30 +0530

    playin around

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 const cors = require('cors');
-const Product = require('./productSchema')
 const UserData = require('./userSchema')
 app.use(express.json());
 app.use(cors());
 app.get('/',(req,res)=>{
     res.send("hello node api")
 })
-mongoose.connect('mongodb+srv://Lakshay:Shagun007returns2@practicemongo.3yk8fpq.mongodb.net/BackendPractice?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://Lakshay:gdschackathonpractice@gdschackathonpractice.myehzxm.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>{
     console.log("connected to mongodb")
     app.listen(3000, ()=>{
@@ -19,16 +18,6 @@ mongoose.connect('mongodb+srv://Lakshay:Shagun007returns2@practicemongo.3yk8fpq.
 }).catch((error)=>{
     console.log(error)
 })
-app.post('/product',async(req,res)=>{
-    console.log(res.body);
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-}) 
-app.get('/products',async(req,res)=>{
-    console.log(res.body);
-    const productt = await Product.find({})
-    res.status(200).json(productt);
-}) 
 app.post('/UserData', async(req,res)=>{
     console.log(res.body);
     const userdata = await UserData.create(req.body);
@@ -38,6 +27,8 @@ app.get('/UserData/:id', async(req,res)=>{
     const data = await UserData.findOne({username: req.params.id})
     res.status(200).json(data);
 })
+
+
 
 
 
--- frontend/practice-app/src/routes/+page.svelte
+++ frontend/practice-app/src/routes/+page.svelte
@@ -61,12 +61,19 @@ let handlelogin = async()=>{
 
 </script>
 <main>
-
+    <label class="flex cursor-pointer gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+        <input type="checkbox" value="dark" class="toggle theme-controller"/>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+      </label>
     <div class="h-screen flex flex-col items-center justify-center ">
-        <p>username:</p>
-    <input bind:value={uusername}/>
-    <p>password:</p>
-    <input bind:value={passwordd} type="password" />
+        <p class="label-text">username:</p>
+    <input bind:value={uusername} type="text" placeholder="Type here" class="input input-bordered input-black w-full max-w-xs"/>
+    <p class="label-text">password: 
+        
+    </p>
+    
+    <input bind:value={passwordd} type="password" placeholder="Type here" class="input input-bordered input-black w-full max-w-xs"/>
     <button on:click={handlelogin} class="btn btn-outline btn-white" >Log in</button>
     <button on:click={handleSubmit} class="btn btn-outline btn-white" >SignUp</button>
     {#if useralready == 1}
